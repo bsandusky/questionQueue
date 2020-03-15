1063b69001cd960721350b71cc5848209eee5435
bsandusky <brett.sandusky@gmail.com>
2020-03-15 12:02:00 -0400

    create stubs for QuestionController and AnswerController methods

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    public function index() 
+    {
+        dd('implement this');
+    }
+
+    public function show(Question $question) 
+    {
+        dd('implement this');
+    }
+
+    public function store()
+    {
+        dd('implement this');
+    }
+}
